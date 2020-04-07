db45726745585e7e48e236294bb885e41fc52341
Arthurferrera <arthur_f.oliveira@hotmail.com>
2020-04-07 00:48:17 -0300

    ajustes + animação

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import {Text, StyleSheet} from 'react-native';
 import {Container, Top, Logo} from './styles';
 import Icon from 'react-native-vector-icons/MaterialIcons';
-import {logo} from '~/assets/Nubank_Logo.png';
+import logo from '~/assets/Nubank_Logo.png';
 
 export default function Header() {
   return (
